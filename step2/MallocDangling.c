b9dec7282414661867393fb318d5be4b52d886e2
selnem <tjdghks0130@naver.com>
2026-04-14 15:29:43 +0000

    dataEnd

--- step2/MallocDangling.c
+++ step2/MallocDangling.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(){
     while(1){
         void *p = malloc(1024 * 1024);
         if(p==NULL) break;
+        memset(p,0,1024*1024);
         sleep(1);
     }
     return 0;
--- step2/MallocFree.c
+++ step2/MallocFree.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<string.h>
 
 int main(){
     while(1){
         void *p = malloc(1024*1024);
         if (p==NULL) break;
         sleep(1);
+        memset(p,0,1024*1024);
         free(p);    
     }
     return 0;
