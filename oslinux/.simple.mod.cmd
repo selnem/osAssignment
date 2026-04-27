0c8cafdcc59fbb3ee5b7a526fe161acd279902f0
selnem <tjdghks0130@naver.com>
2026-04-27 17:26:43 +0000

    isEND

---
+++ oslinux/.simple.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/user2024128055/osAssignment/oslinux/simple.mod := { echo  /home/user2024128055/osAssignment/oslinux/simple.o;  echo; } > /home/user2024128055/osAssignment/oslinux/simple.mod
--- oslinux/simple.c
+++ oslinux/simple.c
@@ -5,14 +5,14 @@
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
+       printk(KERN_INFO "2024128055 ChunSeongHwan: Loading Module\n");
 
        return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+	printk(KERN_INFO "2024128055 ChunSeongHwan: Removing Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
